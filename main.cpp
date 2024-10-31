4344cabe9d2cef7001fc6e5459cd908273f0f719
Lukas Matulis <lukas.matulis@vvk.lt>
2024-10-31 10:41:51 +0200

    main.cpp

--- main.cpp
+++ main.cpp
@@ -19,7 +19,7 @@ void generuotiStudentuDuomenis(const string& failoPavadinimas, int irasuKiekis) 
 
         for (int i = 1; i <= irasuKiekis; ++i) {
             failas << setw(15) << "Vardas" + to_string(i)
-                   << setw(15) << "Pavarde" + to_string(i);
+                << setw(15) << "Pavarde" + to_string(i);
 
             for (int j = 0; j < 5; ++j) {
                 failas << setw(6) << pazymiuDist(gen);
@@ -29,40 +29,47 @@ void generuotiStudentuDuomenis(const string& failoPavadinimas, int irasuKiekis) 
     }
     failas.close();
 }
+void rusiuotiStudentus(const string& inputFailas, const string& badFailas) {
+    deque<Studentas> studentai = nuskaitytiStudentusIsFailo(inputFailas);
 
-void rusiuotiStudentus(const string& inputFailas, const string& goodFailas, const string& badFailas) {
-    vector<Studentas> studentai = nuskaitytiStudentusIsFailo(inputFailas);
-    
-    std::ofstream goodIsrasymas(goodFailas);
     std::ofstream badIsrasymas(badFailas);
-
-    if (!goodIsrasymas.is_open() || !badIsrasymas.is_open()) {
-        throw std::runtime_error("Nepavyko atidaryti failu.");
+    if (!badIsrasymas.is_open()) {
+        throw std::runtime_error("Nepavyko atidaryti failo blogiems studentams.");
     }
 
-    goodIsrasymas << setw(15) << "Pavarde" << setw(20) << "Vardas" << setw(25) << "Vidurkis" << endl;
     badIsrasymas << setw(15) << "Pavarde" << setw(20) << "Vardas" << setw(25) << "Vidurkis" << endl;
 
-    for (const auto& student : studentai) {
-        if (student.gVid() >= 5.0) {
-            goodIsrasymas << setw(15) << student.getPavarde()
-                       << setw(20) << student.getVardas()
-                       << setw(25) << fixed << std::setprecision(2) << student.gVid() << endl;
-        } else {
-            badIsrasymas << setw(15) << student.getPavarde()
-                      << setw(20) << student.getVardas()
-                      << setw(25) << fixed << std::setprecision(2) << student.gVid() << endl;
-        }
+    auto partitionPoint = partition(studentai.begin(), studentai.end(), [](const Studentas& student) {
+        return student.gVid() >= 5.0;
+        });
+
+    for (auto it = partitionPoint; it != studentai.end(); ++it) {
+        badIsrasymas << setw(15) << it->getPavarde()
+            << setw(20) << it->getVardas()
+            << setw(25) << fixed << setprecision(2) << it->gVid() << endl;
+    }
+    badIsrasymas.close();
+
+    studentai.erase(partitionPoint, studentai.end());
+
+    std::ofstream atnaujintasFailas(inputFailas, std::ios::trunc);
+    if (!atnaujintasFailas.is_open()) {
+        throw std::runtime_error("Nepavyko atidaryti Ä¯vesties failo geriems studentams.");
     }
 
-    goodIsrasymas.close();
-    badIsrasymas.close();
-    // Matuoju kiek panaudoja vietos
+    atnaujintasFailas << setw(15) << "Pavarde" << setw(20) << "Vardas" << setw(25) << "Vidurkis" << endl;
+    for (const auto& student : studentai) {
+        atnaujintasFailas << setw(15) << student.getPavarde()
+            << setw(20) << student.getVardas()
+            << setw(25) << fixed << setprecision(2) << student.gVid() << endl;
+    }
+    atnaujintasFailas.close();
+
     parodytiAtminti();
 }
 
 int main() {
-//    Sarasu generavimas
+    //    Sarasu generavimas
     auto genPradzia = std::chrono::high_resolution_clock::now();
     generuotiStudentuDuomenis("studentai_1000.txt", 1000);
     generuotiStudentuDuomenis("studentai_10000.txt", 10000);
@@ -75,15 +82,15 @@ int main() {
     cout << "Generavimo laikas - " << gen_rezas.count() << " sec\n";
 
     auto sortPradzia = std::chrono::high_resolution_clock::now();
-    rusiuotiStudentus("studentai_1000.txt", "studentai_1000_galvociai.txt", "studentai_1000_nuskriaustukai.txt");
-    rusiuotiStudentus("studentai_10000.txt", "studentai_10000_galvociai.txt", "studentai_10000_nuskriaustukai.txt");
-    rusiuotiStudentus("studentai_100000.txt", "studentai_100000_galvociai.txt", "studentai_100000_nuskriaustukai.txt");
-    rusiuotiStudentus("studentai_1000000.txt", "studentai_1000000_galvociai.txt", "studentai_1000000_nuskriaustukai.txt");
-    rusiuotiStudentus("studentai_10000000.txt", "studentai_10000000_galvociai.txt", "studentai_10000000_nuskriaustukai.txt");
+    rusiuotiStudentus("studentai_1000.txt", "studentai_1000_nuskriaustukai.txt");
+    rusiuotiStudentus("studentai_10000.txt", "studentai_10000_nuskriaustukai.txt");
+    rusiuotiStudentus("studentai_100000.txt", "studentai_100000_nuskriaustukai.txt");
+    rusiuotiStudentus("studentai_1000000.txt", "studentai_1000000_nuskriaustukai.txt");
+    rusiuotiStudentus("studentai_10000000.txt", "studentai_10000000_nuskriaustukai.txt");
     auto sortPabaiga = std::chrono::high_resolution_clock::now();
     cout << "Studentai surusiuoti!" << endl;
     std::chrono::duration<float> sort_rezas = sortPabaiga - sortPradzia;
     cout << "Rusiavimo laikas - " << sort_rezas.count() << " sec\n";
-    
+
     return 0;
 }
